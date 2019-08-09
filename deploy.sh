7c963aa9c22b0cc25d304229810bbdf50d958379
Fashola Ayodeji <fashtop3@gmail.com>
2019-08-09 20:07:33 +0100

    configured production

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # install AWS SDK
+# https://haoliangyu.github.io/blog/2018/03/19/AWS-ECS-auto-deployment-with-Travis-CI/
 pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
 
@@ -14,15 +15,14 @@ curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-de
 sudo chmod +x /usr/bin/ecs-deploy
 
 # login AWS ECR
-eval $(aws ecr get-login --region us-east-1)
+eval $(aws ecr get-login --region eu-central-1)
 
 # or login DockerHub
 # docker login --username $DOCKER_HUB_USER --password $DOCKER_HUB_PSW
 
 # build the docker image and push to an image repository
-docker build -t haoliangyu/ecs-auto-deploy .
-docker tag haoliangyu/ecs-auto-deploy:latest $IMAGE_REPO_URL:latest
-docker push $IMAGE_REPO_URL:latest
+docker build -t 686233958969.dkr.ecr.eu-central-1.amazonaws.com/multi-client ./client
+docker push 686233958969.dkr.ecr.eu-central-1.amazonaws.com/multi-client
 
 # update an AWS ECS service with the new image
-ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $IMAGE_REPO_URL:latest
+#ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $IMAGE_REPO_URL:latest
