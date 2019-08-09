69ddc788c15783dc97177e20060debcfe01e5f71
Fashola Ayodeji <fashtop3@gmail.com>
2019-08-09 20:23:36 +0100

    configured production

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# install AWS SDK
+# https://haoliangyu.github.io/blog/2018/03/19/AWS-ECS-auto-deployment-with-Travis-CI/
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
+
+# install necessary dependency for ecs-deploy
+add-apt-repository ppa:eugenesan/ppa
+apt-get update
+apt-get install jq -y
+
+# install ecs-deploy
+curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | \
+  sudo tee -a /usr/bin/ecs-deploy
+sudo chmod +x /usr/bin/ecs-deploy
